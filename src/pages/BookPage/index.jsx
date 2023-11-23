5b4d5e7fd3a75ad88094b8ab34f33daa96317aa7
The Hieu <thehieu236@gmail.com>
2023-11-23 16:18:29 +0700

    responsive book detail page

--- src/pages/BookPage/index.jsx
+++ src/pages/BookPage/index.jsx
@@ -78,38 +78,50 @@ const BookPage = () => {
         Height: "100vh",
       }}
     >
-      <Content className="flex gap-4">
-        <div className="w-1/4 min-h-[300px] bg-white rounded-xl p-3">
-          {images && images.length > 0 ? <ImageGallery items={images} /> : ""}
-        </div>
-        <div className="w-2/4 min-h-[300px] bg-white rounded-xl p-3">
-          <h5 className="m-0 font-thin">Tác giả: abdbd</h5>
-          <h2>Lý Thuyết Trò Chơi</h2>
-          <div>4.7 (41) Đã bán 1433</div>
-          <div className="my-4">118.000₫ -34%</div>
-          <div>Vận chuyển: Miễn phí vận chuyển</div>
-        </div>
-        <div className="w-1/4 min-h-[300px] flex flex-col bg-white rounded-xl p-3">
-          <Space className="flex flex-col items-start flex-1">
-            <Quantity
-              type={"vertical"}
-              value={quantity}
-              onChange={(value) => {
-                console.log(`quantity:`, value);
-                return setQuantity(() => value);
-              }}
+      <Content className="flex max-md:flex-col gap-4">
+        <div className="w-1/4 max-md:w-full min-h-[300px] bg-white rounded-xl p-3">
+          {images && images.length > 0 ? (
+            <ImageGallery
+              showNav={false}
+              autoPlay={false}
+              showFullscreenButton={false}
+              showPlayButton={false}
+              items={images}
             />
-            <h3>Tạm tính:</h3>
-            <span>3,000,300 đ</span>
-          </Space>
+          ) : (
+            ""
+          )}
+        </div>
+        <div className="flex w-full max-sm:flex-col gap-4">
+          <div className="w-2/3 max-md:w-2/3 max-sm:w-full min-h-[300px] bg-white rounded-xl p-3">
+            <h5 className="m-0 font-thin">Tác giả: abdbd</h5>
+            <h2>Lý Thuyết Trò Chơi</h2>
+            <div>4.7 (41) Đã bán 1433</div>
+            <div className="my-4">118.000₫ -34%</div>
+            <div>Vận chuyển: Miễn phí vận chuyển</div>
+          </div>
+          <div className="w-1/3 max-sm:w-full max-md:w-1/3 min-h-[300px] flex flex-col bg-white rounded-xl p-3">
+            <Space className="flex flex-col items-start flex-1">
+              <Quantity
+                type={"vertical"}
+                value={quantity}
+                onChange={(value) => {
+                  console.log(`quantity:`, value);
+                  return setQuantity(() => value);
+                }}
+              />
+              <h3>Tạm tính:</h3>
+              <span>3,000,300 đ</span>
+            </Space>
 
-          <div className="w-full flex flex-col">
-            <Button size="large" type="primary" danger className="w-full">
-              Mua ngay
-            </Button>
-            <Button size="large" className="w-full mt-2">
-              Thêm vào giỏ hàng
-            </Button>
+            <div className="w-full flex justify-center items-center gap-2 sm:flex-col">
+              <Button size="large" type="primary" danger className="w-full">
+                Mua ngay
+              </Button>
+              <Button size="large" className="w-full sm:mt-2">
+                Thêm vào giỏ hàng
+              </Button>
+            </div>
           </div>
         </div>
       </Content>
