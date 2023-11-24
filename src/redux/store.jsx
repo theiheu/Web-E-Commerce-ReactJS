e97758b5f42f0822830bac1f996dbb3e64097ce2
The Hieu <thehieu236@gmail.com>
2023-11-24 11:05:08 +0700

    update redux persist

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,14 +1,44 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { combineReducers } from "redux";
+import {
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
 import accountReducer from "./accountSlice";
 import managerUsersReducer from "./managerUsersSlice";
 import managerBooksReducer from "./managerBooksSlice";
 
-const store = configureStore({
-  reducer: {
-    account: accountReducer,
-    managerUsers: managerUsersReducer,
-    managerBooks: managerBooksReducer,
-  },
+const persistConfig = {
+  key: "root",
+  storage,
+  blacklist: ["account"],
+};
+
+const rootReducer = combineReducers({
+  account: accountReducer,
+  managerUsers: managerUsersReducer,
+  managerBooks: managerBooksReducer,
 });
 
-export default store;
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  // Thêm bất kỳ middleware hoặc cấu hình khác nếu cần thiết
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+const persistor = persistStore(store);
+
+export { store, persistor };
